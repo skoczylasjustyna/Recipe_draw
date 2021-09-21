5d46afc479785d5d226ace37343f30d148c25a87
Justyna Skoczylas <58004529+skoczylasjustyna@users.noreply.github.com>
2021-09-21 21:20:38 +0200

    Add files via upload
    Getting recipe's name and url from main page of Kwestia Smaku using requests and beautifulsoup4, saving it into csv file.

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import re
 import csv
 import pandas as pd
 
+#saving recipies
 def getHTMLdoc(url):
     response = requests.get(url)
     return response.text
 
+#getting main page
 blog_url = "https://www.kwestiasmaku.com"
 blog_html = getHTMLdoc(blog_url)
 soup = bs(blog_html, 'html.parser')
@@ -19,59 +21,51 @@ def getMainLinks():
         main_recipies.append(str(link.get('href')))
 
 getMainLinks()
-#getting rid of items which are not recipies
-del main_recipies[0:324]
-del main_recipies[131:164]
-main_recipies = list(dict.fromkeys(main_recipies))
 
-del main_recipies[46:51]
+#getting rid of items which are not recipies
+main_recipies = list(dict.fromkeys(main_recipies))
 main_recipies.remove("None")
 main_recipies.remove("/user/login")
 main_recipies.remove('#navbar')
-main_recipies.remove("/regulamin-i-polityka-prywatnosci")
 
-rec_url = []
+del main_recipies[0:290]
+del main_recipies[47:72]
+del main_recipies[-1]
+
+#lists to store recipes name and url
+full_rec_url = []
 rec_names = []
+rec_urls = []
 
-#splitting record so that only recipe's name remains, saving pages url
+#splitting record so that only recipe's name remains
 def create_rec_name():
     for i in range(len(main_recipies)):
         x = main_recipies[i]
-        rec_url.append(x)
+        rec_urls.append(x)
         splitting = x.split('/')
-        rec_name = splitting[2]
-        rec_name= rec_name.replace("-", " ")
-        rec_name = rec_name.replace("_", " ")
-        rec_names.append(rec_name)
+        if len(splitting) >2:
+            name = splitting[2]
+            name= name.replace("-", " ")
+            name = name.replace("_", " ")
+            rec_names.append(name)
+        else:
+            pass
 
 create_rec_name()
 
 #creating valid url for recipes
-full_rec_url = []
+
 def create_url():
-    for i in range(len(rec_url)):
-        recipe_url = blog_url +  rec_url[i]
-        full_rec_url.append(recipe_url)
+    for i in range(len(rec_urls)):
+        url = blog_url + rec_urls[i]
+        full_rec_url.append(url)
 
 create_url()
-#getting ingredients from single recipe - section in progress - it should take ingredients for each of recipies saved into list, so that they can be added to the csv
-ing_list = []
-def get_ing():
-    for i in range(len(full_rec_url)):
-        ing = full_rec_url[i]
-        recipe_html = requests.get(ing).content
-
-        data = bs(recipe_html, 'html.parser')
-        div_data = data.find_all("div", {"class": "field field-name-field-skladniki field-type-text-long field-label-hidden"})
-
-        for x in div_data:
-            li_tag = x.find_all("li")
-            for x in li_tag:
-                print(x.text)
+full_rec_url.remove('https://www.kwestiasmaku.com/home-przepisy?page=1')
 
 #saving recipes name and url into csv file
+
 dictionary = {'name' : rec_names, 'url' : full_rec_url}
 dataframe = pd.DataFrame(dictionary)
 dataframe.to_csv('recipes.csv')
 
-#getting random recipe - in progress
